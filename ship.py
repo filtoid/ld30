8f5efa4f7a2ba477b2913a72092bb12a7c09e597
filtoid <philjeffes@hotmail.com>
2014-08-23 09:46:05 +0100

    Made the ship draw

--- ship.py
+++ ship.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 
 class Ship(object):
 	def __init__(self):
@@ -7,8 +7,21 @@ class Ship(object):
 		self.rot = 0
 		
 	def draw(self, screen):
+		
+		pt1 = [20 * math.sin(self.rot) + self.loc['x'], 20 * math.cos(self.rot) + self.loc['y']]
+		pt2 = [10 * math.sin(self.rot +  (3*(math.pi)/4)) + self.loc['x'], 
+				10 * math.cos(self.rot + (3 *(math.pi)/4)) + self.loc['y']]
+		pt3 = [10 * math.sin(self.rot +  (5*(math.pi)/4)) + self.loc['x'], 
+				10 * math.cos(self.rot + (5 *(math.pi)/4)) + self.loc['y']]
+		
+		pygame.draw.line(screen, (255, 255, 255), (pt1[0], pt1[1]),
+			(pt2[0],pt2[1]))
+		pygame.draw.line(screen, (255, 255, 255), (pt2[0], pt2[1]),
+			(self.loc['x'], self.loc['y']))
 		pygame.draw.line(screen, (255, 255, 255), (self.loc['x'], self.loc['y']),
-			(self.loc['x']+100, self.loc['y']))
+			(pt3[0],pt3[1]))
+		pygame.draw.line(screen, (255, 255, 255), (pt3[0], pt3[1]),
+			(pt1[0],pt1[1]))
 		
 	def update(self):
 		pass
